b72e2fb563dd1dacc316f97c6fe0b5a3224f741d
Yurii Skarbek <yurii.skarbek@gmail.com>
2019-07-02 17:30:18 +0300

    [0.1.2] Added checking the exchange rate for error;
    Hide block with currency from header, if we cannot get exchange rate from PrivatBank APi.

--- scripts/main.js
+++ scripts/main.js
@@ -2,23 +2,28 @@
 let exchangeRatesApiUrl = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5';
 
 fetch(exchangeRatesApiUrl)
-.then(res => res.json())
-.then((exchangeRatesData) => {
-    var EUR_sale = parseFloat(exchangeRatesData[1].sale).toFixed(2);
-    var EUR_buy = parseFloat(exchangeRatesData[1].buy).toFixed(2);
-    document.getElementById('exchangerate_EUR-sale').innerHTML = EUR_sale;
-    document.getElementById('exchangerate_EUR-buy').innerHTML = EUR_buy;
+    .then(res => res.json())
+    .then((exchangeRatesData) => {
+        var EUR_sale = parseFloat(exchangeRatesData[1].sale).toFixed(2);
+        var EUR_buy = parseFloat(exchangeRatesData[1].buy).toFixed(2);
+        document.getElementById('exchangerate_EUR-sale').innerHTML = EUR_sale;
+        document.getElementById('exchangerate_EUR-buy').innerHTML = EUR_buy;
 
-    var USD_sale = parseFloat(exchangeRatesData[0].sale).toFixed(2);
-    var USD_buy = parseFloat(exchangeRatesData[0].buy).toFixed(2);
-    document.getElementById('exchangerate_USD-sale').innerHTML = USD_sale;
-    document.getElementById('exchangerate_USD-buy').innerHTML = USD_buy;
+        var USD_sale = parseFloat(exchangeRatesData[0].sale).toFixed(2);
+        var USD_buy = parseFloat(exchangeRatesData[0].buy).toFixed(2);
+        document.getElementById('exchangerate_USD-sale').innerHTML = USD_sale;
+        document.getElementById('exchangerate_USD-buy').innerHTML = USD_buy;
 
-    var RUR_sale = parseFloat(exchangeRatesData[2].sale).toFixed(2);
-    var RUR_buy = parseFloat(exchangeRatesData[2].buy).toFixed(2);
-    document.getElementById('exchangerate_RUR-sale').innerHTML = RUR_sale;
-    document.getElementById('exchangerate_RUR-buy').innerHTML = RUR_buy;
-}).catch(err => { throw err });
+        var RUR_sale = parseFloat(exchangeRatesData[2].sale).toFixed(2);
+        var RUR_buy = parseFloat(exchangeRatesData[2].buy).toFixed(2);
+        document.getElementById('exchangerate_RUR-sale').innerHTML = RUR_sale;
+        document.getElementById('exchangerate_RUR-buy').innerHTML = RUR_buy;
 
+        // show exchange rates in header if request was success
+        document.getElementById('exchangerates').style.display = 'block';
+    }).catch(err => {
+        console.error('cannot get exchange rates from PrivatBank APi')
+        document.getElementById('exchangerates').style.display = 'none';
+    });
 
-
+    console.log('RUR: ', this.RUR_buy)
