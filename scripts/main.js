9ebbccc522459260c14e406b0c6805a1e16088ee
Yurii Skarbek <yurii.skarbek@gmail.com>
2019-07-02 15:55:25 +0300

    [0.1.1] Upgraded exchange rates;
    Now you can see exchange rate for sale and buy currency in header;

--- scripts/main.js
+++ scripts/main.js
@@ -4,17 +4,20 @@ let exchangeRatesApiUrl = 'https://api.privatbank.ua/p24api/pubinfo?json&exchang
 fetch(exchangeRatesApiUrl)
 .then(res => res.json())
 .then((exchangeRatesData) => {
-    var EUR = exchangeRatesData[1].sale;
-    var EUR = parseFloat(EUR).toFixed(2);
-    document.getElementById('exchangerate_EUR').innerHTML = EUR;
+    var EUR_sale = parseFloat(exchangeRatesData[1].sale).toFixed(2);
+    var EUR_buy = parseFloat(exchangeRatesData[1].buy).toFixed(2);
+    document.getElementById('exchangerate_EUR-sale').innerHTML = EUR_sale;
+    document.getElementById('exchangerate_EUR-buy').innerHTML = EUR_buy;
 
-    var USD = exchangeRatesData[0].sale;
-    var USD = parseFloat(USD).toFixed(2);
-    document.getElementById('exchangerate_USD').innerHTML = USD;
+    var USD_sale = parseFloat(exchangeRatesData[0].sale).toFixed(2);
+    var USD_buy = parseFloat(exchangeRatesData[0].buy).toFixed(2);
+    document.getElementById('exchangerate_USD-sale').innerHTML = USD_sale;
+    document.getElementById('exchangerate_USD-buy').innerHTML = USD_buy;
 
-    var RUR = exchangeRatesData[2].sale;
-    var RUR = parseFloat(RUR).toFixed(2);
-    document.getElementById('exchangerate_RUR').innerHTML = RUR;
+    var RUR_sale = parseFloat(exchangeRatesData[2].sale).toFixed(2);
+    var RUR_buy = parseFloat(exchangeRatesData[2].buy).toFixed(2);
+    document.getElementById('exchangerate_RUR-sale').innerHTML = RUR_sale;
+    document.getElementById('exchangerate_RUR-buy').innerHTML = RUR_buy;
 }).catch(err => { throw err });
 
 
