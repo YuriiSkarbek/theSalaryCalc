2ea17a0a4fb448c61c15c2e1c59ae62d9d54fe2a
Yurii Skarbek <yurii.skarbek@gmail.com>
2019-09-02 15:34:08 +0300

    [0.2.8] Fixed displaying exchange rates in header;
    Added some comments in index.html.

--- scripts/main.js
+++ scripts/main.js
@@ -6,23 +6,23 @@ fetch(exchangeRatesApiUrl)
     .then((exchangeRatesData) => {
         var EUR_sale = parseFloat(exchangeRatesData[1].sale).toFixed(2);
         var EUR_buy = parseFloat(exchangeRatesData[1].buy).toFixed(2);
-        document.getElementById('exchangerate_EUR-sale').innerHTML = EUR_sale;
-        document.getElementById('exchangerate_EUR-buy').innerHTML = EUR_buy;
+        document.getElementById('exchangerate-EUR-sale').innerHTML = EUR_sale;
+        document.getElementById('exchangerate-EUR-buy').innerHTML = EUR_buy;
 
         var USD_sale = parseFloat(exchangeRatesData[0].sale).toFixed(2);
         var USD_buy = parseFloat(exchangeRatesData[0].buy).toFixed(2);
-        document.getElementById('exchangerate_USD-sale').innerHTML = USD_sale;
-        document.getElementById('exchangerate_USD-buy').innerHTML = USD_buy;
+        document.getElementById('exchangerate-USD-sale').innerHTML = USD_sale;
+        document.getElementById('exchangerate-USD-buy').innerHTML = USD_buy;
 
         var RUR_sale = parseFloat(exchangeRatesData[2].sale).toFixed(2);
         var RUR_buy = parseFloat(exchangeRatesData[2].buy).toFixed(2);
-        document.getElementById('exchangerate_RUR-sale').innerHTML = RUR_sale;
-        document.getElementById('exchangerate_RUR-buy').innerHTML = RUR_buy;
+        document.getElementById('exchangerate-RUR-sale').innerHTML = RUR_sale;
+        document.getElementById('exchangerate-RUR-buy').innerHTML = RUR_buy;
 
         // show exchange rates in header if request was success
         document.getElementById('exchange-rates').style.display = 'block';
     }).catch(err => {
-        console.error('cannot get exchange rates from PrivatBank APi')
+        console.error('cannot get exchange rates from PrivatBank APi. Error:', err)
         document.getElementById('exchange-rates').style.display = 'none';
     });
 
