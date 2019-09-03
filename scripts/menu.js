07e4496ac29ccc93577045ab3268e7aa87e489da
Yurii Skarbek <yurii.skarbek@gmail.com>
2019-09-03 15:57:25 +0300

    [0.2.11] Added responsive to sidebar;

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,4 +1,5 @@
-function getOfficialSalary(){
+// functions for changing main content after clicking to menu link
+function getOfficialSalary() {
     document.getElementById("calc-official__btn").classList.add("sidebar__btn-active");
     document.getElementById("calc-non-official__btn").classList.remove("sidebar__btn-active");
 
@@ -6,10 +7,37 @@ function getOfficialSalary(){
     document.getElementById("calc-non-official").style.display = 'none';
 }
 
-function getNonOfficialSalary(){
+function getNonOfficialSalary() {
     document.getElementById("calc-non-official__btn").classList.add("sidebar__btn-active");
     document.getElementById("calc-official__btn").classList.remove("sidebar__btn-active");
-    
+
     document.getElementById("calc-non-official").style.display = 'block';
     document.getElementById("calc-official").style.display = 'none';
+}
+
+
+// fuction for opening and closing sidebar
+var isSideBarOpen = false;
+function openSidebar() {
+    if (screen.width <= 420) {
+        document.getElementById("sidebar").style.width = "100%";
+    } else {
+        document.getElementById("sidebar").style.width = "300px";
+    }
+    isSideBarOpen = true;
+}
+
+function closeSidebar() {
+    document.getElementById("sidebar").style.width = "0";
+    isSideBarOpen = false;
+
+}
+
+function toggleSidebar(){
+    if(isSideBarOpen == false){
+        openSidebar();
+    }
+    else{
+        closeSidebar();
+    }
 }
